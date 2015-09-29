1dd67108cf46fc474364bf8df12a5e351377ef7b
Gergely Papp <gergely.papp@itworks.hu>
2015-09-29 21:23:14 +0200

    Added register.sh

--- register.sh
+++ register.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 IP=$(ip addr show dev eth0 | awk '/inet / {print $2}'| cut -d/ -f1)
-VALUE='{"name":"ui","path":"*","authorized":false,"host":"'$IP'","port":"2003","url":"http://'$IP':2003/file"}'
+VALUE='{"name":"ui","path":"*","authorized":false,"host":"'$IP'","port":"80","url":"http://'$IP'/"}'
 watch -n5 -x curl http://192.168.1.20:2379/v2/keys/mindweb/${TYPE}/services/ui -XPUT \
   -d ttl=5 \
   -d value=${VALUE}
